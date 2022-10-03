00bc724bb467586ce0d24a74509c5ea240ccd421
brzan <bolber1010@gmail.com>
2022-10-03 23:03:55 +0300

    deployment preparing

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -27,7 +27,7 @@ const Detail = ({ exerciseDetail }) => {
     <Stack
       gap='60px'
       sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}>
-      <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
+      <img src={gifUrl} alt={name} className='detail-image' />
       <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
         <Typography
           sx={{ fontSize: { lg: "64px", xs: "30px" } }}
